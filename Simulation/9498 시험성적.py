629689558e590524c96099af70e92e3ca95496f8
YEJI-324 <kej980324@gmail.com>
2024-05-12 17:42:53 +0900

    백준 9498 시험성적

---
+++ Simulation/9498 시험성적.py
@@ -0,0 +1,10 @@
+# 9498 시험성적
+# 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F
+
+result = int(input())
+
+if result >= 90: print('A')
+elif result >= 80: print('B')
+elif result >= 70: print('C')
+elif result >= 60: print('D')
+else: print('F')
